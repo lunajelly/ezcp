f3eb23319a7ab67ccb7d44b5e82e3a4f2b2241f9
lunajelly <luna_jellyfishies@mail.com>
2025-12-15 01:20:34 -0500

    quickdecl gussy/refactor for easier msg updates

---
+++ ali-util.js
@@ -0,0 +1,26 @@
+// ------Alison's tiny utility js lib V.1.2------
+// eff utility wrapper fns/lib for my dex issues 
+// style: keep 1 underlying longname fn per fn, & underlying fn that shd be obvious
+function id(id) {
+	return document.getElementById(id);
+}
+
+function qs(selector) {
+	return document.querySelector(selector);
+}
+
+function qsa(selector) {
+	return document.querySelectorAll(selector);
+}
+
+function l(msg) {
+	console.log(msg);
+}
+
+function a(msg) {
+	alert(msg);
+}
+
+function e(msg) {
+	throw new Error(msg);
+}
