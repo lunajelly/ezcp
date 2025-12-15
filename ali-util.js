a8413958a027c9c8419b89a62ed5becbaabfe4a7
lunajelly <luna_jellyfishies@mail.com>
2025-12-15 02:26:21 -0500

    gussy/refactor

--- ali-util.js
+++ ali-util.js
@@ -1,6 +1,6 @@
 // ------Alison's tiny utility js lib V.1.2------
 // eff utility wrapper fns/lib for my dex issues 
-// style: keep 1 underlying longname fn per fn, & underlying fn that shd be obvious
+// style: keep 1 underlying longname fn per fn, & underlying fn that shd be obvious. no special characters, they hurt
 function id(id) {
 	return document.getElementById(id);
 }
